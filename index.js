ff27beea1c71eb239c6d971165f1ef4d2653eb0c
aptSharp <afonso.raffael@gmail.com>
2024-06-12 16:15:32 -0300

    101 DOM

---
+++ index.js
@@ -0,0 +1,7 @@
+const text = document.querySelector('p'); //querySelectorAll diferente de querySelector
+
+text.innerHTML = "testeJS";
+
+text.style.color = '#f33';
+
+// para a manipulação do DOM pode ser feito tudo dentro de uma tag do html5 chamada <script> </script>, tem a mesma função de um arquivo js como anexo.
