e4453f0454055faef86bdff47150b72170bc15bf
aptSharp <afonso.raffael@gmail.com>
2024-06-17 14:17:01 -0300

    exemplos de utilização.

--- index.js
+++ index.js
@@ -4,4 +4,5 @@ text.innerHTML = "testeJS";
 
 text.style.color = '#f33';
 
-// para a manipulação do DOM pode ser feito tudo dentro de uma tag do html5 chamada <script> </script>, tem a mesma função de um arquivo js como anexo.
+// para a manipulação do DOM pode ser feito tudo dentro de uma tag do html5 chamada <script> </script>,
+//tem a mesma função de um arquivo js como anexo.
